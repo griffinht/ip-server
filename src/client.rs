d45027fb24513250354a1675813a018873239982
stzups <griffinht@gmail.com>
2021-11-20 23:10:49 -0500

    client error handling

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,5 @@
 use std::io::{Read, Write};
+use std::net::TcpStream;
 
 pub fn connect<A: std::net::ToSocketAddrs>(address: A) -> Result<(), i32> {
     let mut stream = match std::net::TcpStream::connect(address) {
@@ -8,38 +9,38 @@ pub fn connect<A: std::net::ToSocketAddrs>(address: A) -> Result<(), i32> {
 
     eprintln!("connected to {}", stream.peer_addr().unwrap());
 
-    stream.write(&[0u8, 1] );
-    let address_type = &mut [0u8, 1];
-    stream.read_exact(address_type);
-    match address_type[0] {
-        0 => {
-            let buffer = &mut [0u8; 4];
-            match stream.read_exact(buffer) {
-                Ok(()) => {
-                    eprintln!("read");
-                    let address = std::net::IpAddr::from(*buffer);
-                    eprintln!("{}", address.to_string())
-                }
-                Err(error) => { eprintln!("{}", error); return Err(1) }
-            };
-        }
-        1 => {
-            let buffer = &mut [0u8; 16];
-            match stream.read_exact(buffer) {
-                Ok(()) => {
-                    eprintln!("read");
-                    let address = std::net::IpAddr::from(*buffer);
-                    eprintln!("{}", address.to_string())
-                }
-                Err(error) => { eprintln!("{}", error); return Err(1) }
-            };
-        }
-        _ => {
-            eprintln!("unknown address type");
-            return Err(1)
-        }
-    }
+    match stream.write(&[0u8, 1] ) {
+        Ok(_) => {}
+        Err(error) => { eprintln!("error writing\n{}", error); return Err(1) }
+    };
+    match read(stream) {
+        Ok(_) => {}
+        Err(error) => { eprintln!("error reading\n{}", error); return Err(1) }
+    };
 
 
     return Ok(());
+}
+
+fn read(mut stream: TcpStream) -> std::io::Result<()> {
+    let address_type = &mut [0u8, 1];
+    stream.read_exact(address_type)?;
+    match address_type[0] {
+        0 => {
+            let buffer = &mut [0u8; 4];
+            stream.read_exact(buffer)?;
+            let address = std::net::IpAddr::from(*buffer);
+            println!("{}", address.to_string())
+        }
+        1 => {
+            let buffer = &mut [0u8; 16];
+            stream.read_exact(buffer)?;
+            let address = std::net::IpAddr::from(*buffer);
+            println!("{}", address.to_string())
+        }
+        _ => {
+            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "unknown address type"))
+        }
+    }
+    Ok(())
 }
