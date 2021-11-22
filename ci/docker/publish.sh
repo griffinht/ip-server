bcee20b6b3071f49fbdbe8e97052d974384f64d6
stzups <griffinht@gmail.com>
2021-11-22 13:50:52 -0500

    add more variables

--- ci/docker/publish.sh
+++ ci/docker/publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 USER=$1
-PASSWORD=$2
+NAME=$2
+VERSION=$3
+PASSWORD=$4
 
-docker build . --tag ip-server:latest
+docker build . --tag "$USER"/"$NAME":"$VERSION"
 echo "$PASSWORD" | docker login -u "$USER" --password-stdin
-docker push "$USER"/ip-server:latest
+docker push "$USER"/"$NAME":"$VERSION"
