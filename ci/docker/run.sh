b6e0de78335065dbda8146d42567cca6aacd2120
stzups <griffinht@gmail.com>
2021-11-22 11:55:14 -0500

    remove docker in docker

--- ci/docker/run.sh
+++ ci/docker/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd ../..
-docker build . -f ci/docker/Dockerfile --tag ip-server-docker:latest
-docker run  -e "DOCKER_REGISTRY_USER=$1" -e "DOCKER_REGISTRY_PASSWORD=$2" ip-server-docker
+./ci/docker/publish.sh "$1" "$2"
