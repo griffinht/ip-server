1bef046559b174b8d916ccf7eb1fd8ec5e05956d
stzups <griffinht@gmail.com>
2021-11-22 22:20:03 -0500

    rerename, fix typo

--- ci/cargo/run.sh
+++ ci/cargo/run.sh
@@ -3,5 +3,5 @@ set -e
 
 TOKEN=$1
 
-docker build . -f ci/cargo/Dockerfile --tag cargo-docker:latest
+docker build . -f ./ci/cargo/Dockerfile --tag cargo-docker:latest
 docker run -e "CARGO_TOKEN=$TOKEN" cargo-docker:latest
--- ci/docker/build-and-push.sh
+++ ci/docker/build-and-push.sh
@@ -6,6 +6,6 @@ NAME=$2
 VERSION=$3
 PASSWORD=$4
 
-docker build --build-arg CARGO_VERSION="$VERSION" -f ci/docker/Dockerfile --tag "$USER"/"$NAME":"$VERSION" .
+docker build --build-arg CARGO_VERSION="$VERSION" -f ./cd/docker/Dockerfile --tag "$USER"/"$NAME":"$VERSION" .
 echo "$PASSWORD" | docker login -u "$USER" --password-stdin
 docker push "$USER"/"$NAME":"$VERSION"
