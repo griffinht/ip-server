458d2a376e133d6a0fd441d2ffc08a1ca9756ebc
stzups <griffinht@gmail.com>
2021-11-22 19:10:30 -0500

    simplify, add test-server-client.sh

--- ci/docker/docker-compose-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ci/docker/docker-compose.yml up
---
+++ ci/docker/test-server-client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# starts server and connects client to server
+USER=$1
+NAME=$2
+VERSION=$3
+
+SERVER_ID=$(docker run --rm -p 8000:8000 --detach "$USER/$NAME:$VERSION")
+docker run --rm --network=host "$USER/$NAME:$VERSION" --client 127.0.0.1:8000
+CLIENT_EXIT_CODE=$?
+docker rm "$SERVER_ID" --force > /dev/null
+exit "$CLIENT_EXIT_CODE"
