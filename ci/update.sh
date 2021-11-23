69c0a82ea36878abbe080c379accfd7cc80ce4ad
stzups <griffinht@gmail.com>
2021-11-22 23:17:50 -0500

    start to add update.sh

---
+++ ci/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+VERSION=$1
+
+if [[ $(git status --porcelain) ]]; then
+  echo "git status shows changes, please commit/discard these changes to continue"
+  exit 1
+fi
+
+OLD_VERSION=$(./ci/cargo-metadata.sh version)
+
+if [[ "$VERSION" == "$OLD_VERSION" ]]; then
+  echo "Cargo.toml is already version $VERSION"
+  exit 1
+fi
+
+sed -i "s/$OLD_VERSION/$VERSION/g" Cargo.toml
+exit 0
+git tag "$VERSION"
+git add Cargo.toml
+git commit -m "version $OLD_VERSION -> $VERSION"
+git push --tags
