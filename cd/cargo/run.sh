588a388d7a891d79487388e345b44c98f37d497e
stzups <griffinht@gmail.com>
2021-11-22 21:46:49 -0500

    fix missing tag and rename

--- cd/cargo/run.sh
+++ cd/cargo/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ../..
-docker build . -f ci/cargo/Dockerfile --tag ip-server-cargo:latest
-docker run -e "CARGO_TOKEN=$1" ip-server-cargo
+docker build . -f ci/cargo/Dockerfile --tag cargo-publish:latest
+docker run -e "CARGO_TOKEN=$1" cargo-publish:latest
