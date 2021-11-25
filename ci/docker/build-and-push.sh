b57f555600bbaf89c2f91ef6d6db232eb3c0f4d6
stzups <griffinht@gmail.com>
2021-11-25 12:07:51 -0500

    test splitting gh action

--- ci/docker/build-and-push.sh
+++ ci/docker/build-and-push.sh
@@ -5,7 +5,8 @@ USER=$1
 NAME=$2
 VERSION=$3
 PASSWORD=$4
+REGISTRY=$5
 
 docker build --build-arg CARGO_VERSION="$VERSION" -f ./ci/docker/Dockerfile --tag "$USER/$NAME:$VERSION" .
 echo "$PASSWORD" | docker login -u "$USER" --password-stdin
-docker push "$USER"/"$NAME":"$VERSION"
+docker push "$REGISTRY/$USER/$NAME:$VERSION"
