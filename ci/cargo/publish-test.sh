7363d92af5aad92e68e73fc506ba3f618bd37e33
stzups <griffinht@gmail.com>
2021-11-25 20:51:51 -0500

    remove debug

--- ci/cargo/publish-test.sh
+++ ci/cargo/publish-test.sh
@@ -4,7 +4,6 @@ set -e
 CARGO_TOKEN=$1
 
 docker build . -f ./ci/cargo/publish/Dockerfile --tag ip-server-cargo-publish:latest
-printf "\n\n\nn\n\n$CARGO_TOKEN"
 docker run --rm ip-server-cargo-publish:latest "$CARGO_TOKEN"
 
 docker build . -f ./ci/cargo/install/Dockerfile --tag ip-server-cargo-install:latest --build-arg "VERSION=$(./ci/cargo-metadata.sh version)"
